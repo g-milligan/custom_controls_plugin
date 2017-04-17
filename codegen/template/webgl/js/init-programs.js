1684ac98a0a0c9511c1b2afdd74240f5dde9292c
Gregg Milligan II <mail.gtm2@gmail.com>
2017-04-17 17:53:00 -0400

    init programs generator

--- codegen/template/webgl/js/init-programs.js
+++ codegen/template/webgl/js/init-programs.js
@@ -1,3 +1,18 @@
 function codeGen_initPrograms(txt, args){
+  //GENERATE CODE
 
+  var progsCode='//create and use a programs\n';
+
+  //for each program
+  args.cg.children('program', args.vals, function(progJson, p){
+    var pn=progJson.name.val;
+
+    progsCode+="var prog_"+pn+"=getProgram('#vs-"+pn+":first', '#fs-"+pn+":first');\n";
+    progsCode+="gl.useProgram(prog_"+pn+"['program']);\n\n";
+  });
+
+  //INSERT THE PROGRAMS' CODE INTO txt
+
+  txt=progsCode;
+  return {txt:txt};
 }
--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -31,10 +31,10 @@ function codeGen_initJs(txt, args){
 
   var noDepthTest='//', noObscureDistance='//';
   if(args.vals['depth-test'].val){
-    noDepthTest=''
+    noDepthTest='';
   }
   if(args.vals['obscure-distance'].val){
-    noObscureDistance=''
+    noObscureDistance='';
   }
 
   txt+='//set background color and depth\n';
@@ -48,7 +48,22 @@ function codeGen_initJs(txt, args){
 }
 
 function codeGen_initPrograms(txt, args){
+  //GENERATE CODE
 
+  var progsCode='//create and use a programs\n';
+
+  //for each program
+  args.cg.children('program', args.vals, function(progJson, p){
+    var pn=progJson.name.val;
+
+    progsCode+="var prog_"+pn+"=getProgram('#vs-"+pn+":first', '#fs-"+pn+":first');\n";
+    progsCode+="gl.useProgram(prog_"+pn+"['program']);\n\n";
+  });
+
+  //INSERT THE PROGRAMS' CODE INTO txt
+
+  txt=progsCode;
+  return {txt:txt};
 }
 
 function codeGen_vertexShaders(txt, args){
