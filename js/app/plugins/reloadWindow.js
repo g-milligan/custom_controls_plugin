b57494dd0c9a3a42cbfcf8614e3f740d1f8aeae4
Gregg Milligan II <mail.gtm2@gmail.com>
2017-04-23 15:53:48 -0400

    working on test window

--- js/app/plugins/reloadWindow.js
+++ js/app/plugins/reloadWindow.js
@@ -28,11 +28,57 @@ var reloadWindow=(function(){
       };
       loadUpDefaultArgs(lookForArgs, args);
 
+      var createPathLookupByKey=function(urlArray, callback){
+        var ret;
+        if(urlArray!=undefined && urlArray.length>0){
+          ret={};
+          for(var u=0;u<urlArray.length;u++){
+            var ujson=urlArray[u]; var key=ujson['key'];
+            ret[key]=ujson;
+            if(callback!=undefined){
+              callback(key, ujson, u);
+            }
+          }
+        } return ret;
+      };
 
-      
+
+      ret['reload']=function(urlKey, vals){
+        var res={status:'error, could not get data for key, "'+urlKey+'"'};
+        var urlPaths=self['get_url_paths'](vals);
+        var urls_lookup=createPathLookupByKey(urlPaths);
+        if(urls_lookup.hasOwnProperty(urlKey)){
 
 
 
+
+
+
+
+
+        }
+        return res;
+      };
+
+      ret['open']=function(urlKey, vals){
+        var res={status:'error, could not get data for key, "'+urlKey+'"'};
+        var urlPaths=self['get_url_paths'](vals);
+        var urls_lookup=createPathLookupByKey(urlPaths);
+        if(urls_lookup.hasOwnProperty(urlKey)){
+
+
+
+
+
+
+
+
+        }
+        return res;
+      };
+
+      self['reload']=ret['reload'];
+      self['open']=ret['open'];
 
       return ret;
     }
